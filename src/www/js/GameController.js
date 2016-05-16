3512c937bec551346e515bfdd995121a7bbb4606
Krockema <martin.krockert@gmail.com>
2016-05-16 13:20:36 +0200

    first simple logic
    map, coin, controller and first animation

--- src/www/js/GameController.js
+++ src/www/js/GameController.js
@@ -1,8 +1,14 @@
 app.controller('GameController', function ($scope, $state) {
+    $scope.map = new Map(6, 6);
 
     $scope.back = function () {
         console.log('hallo game');
         //$location.path('/start');
         $state.go('start');
+    }
+
+    $scope.insertCoin = function(x, y) {
+        console.log('insertCoin');
+        $scope.map.applyCoin(new Coin(x, y, 'me'));
     }
 });
---
+++ src/www/js/coin.js
@@ -0,0 +1,9 @@
+class Coin {
+    constructor(x, y, owner) {
+    this.x = x;
+    this.y = y;
+    this.owner = owner;
+    this.color = 'red';
+    this.direction;
+    }
+}
