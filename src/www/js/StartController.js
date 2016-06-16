cd4e3d532371a4d9458c51be9a7d256b0a633177
Oliver Guhr <oliver.guhr@gmx.de>
2016-06-16 11:03:24 +0200

    local persistant store added

--- src/www/js/StartController.js
+++ src/www/js/StartController.js
@@ -5,7 +5,6 @@ app.controller('StartController', function($scope, $state, socket, SessionServic
   $scope.waitingForMatch = false;
   $scope.editSettings = false;
   var userId = "_" + Math.random();
-
   socket.on('user.count', function(message) {
     $scope.usercount = message.users;
     console.log(message.users + " Users");
@@ -15,7 +14,7 @@ app.controller('StartController', function($scope, $state, socket, SessionServic
       var message = { name: $scope.yourName, coinsToSolve: $scope.coinsToSolve };
       socket.emit("match.request", message);
       $scope.waitingForMatch = true;
-      SessionService.attachUser("1", $scope.yourName, $scope.coinsToSolve, $scope.yourName, $scope.yourName, false);
+
   }
 
   $scope.cancelRequest = function() {
@@ -25,9 +24,9 @@ app.controller('StartController', function($scope, $state, socket, SessionServic
 
   socket.on('match.found', function(message) {
     if (message.playerA.name === $scope.yourName) {
-      SessionService.attachUser(message.playerA.id, $scope.yourName, $scope.coinsToSolve, message.playerB.name, message.playerA.name, false);
+      SessionService.attachUser(message.playerA.id, $scope.yourName, $scope.coinsToSolve, message.playerB.name, message.playerA.name);
     } else {
-      SessionService.attachUser(message.playerB.id, $scope.yourName, $scope.coinsToSolve, message.playerA.name, message.playerA.name, false);
+      SessionService.attachUser(message.playerB.id, $scope.yourName, $scope.coinsToSolve, message.playerA.name, message.playerA.name);
     }
     $scope.coinsToSolve = message.coinsToSolve;
     $state.go('game');
@@ -36,7 +35,7 @@ app.controller('StartController', function($scope, $state, socket, SessionServic
   // on settingschange push it to the SessionService
   $scope.save = function() {
     $scope.editSettings = false;
-    SessionService.attachUser(userId, $scope.yourName, $scope.coinsToSolve, "Bob", $scope.yourName, true);
+    SessionService.attachUser(userId, $scope.yourName, $scope.coinsToSolve, "Bob", $scope.yourName);
 
   };
 });
