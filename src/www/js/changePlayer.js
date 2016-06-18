e183cdd2dd5d66d745082301138e59c31fbd2900
SeifertS <Sonne.Seifert@web.de>
2016-06-18 16:44:53 +0200

    Highlights active player

--- src/www/js/changePlayer.js
+++ src/www/js/changePlayer.js
@@ -3,16 +3,12 @@ function changePlayer(currentPlayer,player1) {
 
   if (currentPlayer==player1) {
 
-    $(document.getElementById("red")).velocity({
-      transformOrigin: "50% 50%"
-    });
-    $(document.getElementById("blue")).velocity({
-      scale: 1.0
-    },500);
-    $(document.getElementById("red")).velocity({
+    document.getElementById("redback").setAttribute('opacity','1');
+    document.getElementById("blueback").setAttribute('opacity','0');
+    /*$(document.getElementById("red")).velocity({
       scale: 1.3
     },500);
-
+*/
     (document.getElementById("arrowpath")).style="fill:red";
     (document.getElementById("optcircle")).style="stroke:red";
     (document.getElementById("optnumber")).style="fill:red";
@@ -20,15 +16,9 @@ function changePlayer(currentPlayer,player1) {
     (document.getElementById("arrowpath")).style="fill:blue";
     (document.getElementById("optcircle")).style="stroke:blue";
     (document.getElementById("optnumber")).style="fill:blue";
-    $(document.getElementById("blue")).velocity({
-      transformOrigin: "50% 50%"
-    });
-    $(document.getElementById("red")).velocity({
-      scale: 1.0
-    },500);
-    $(document.getElementById("blue")).velocity({
-      scale: 1.3
-    },500);
+
+    document.getElementById("redback").setAttribute('opacity','0');
+    document.getElementById("blueback").setAttribute('opacity','1');
 
   }
   if (!Timer.timerRdy) {
